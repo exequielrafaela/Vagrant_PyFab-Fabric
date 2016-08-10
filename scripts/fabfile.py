a8078d1326e3bc2c28616d5aa91cd3d786f2ceb8
exequielrafaela <exequielrafaela@gmail.com>
2016-08-10 16:22:52 -0300

    minor changes - the test_key task (function) now expects the argument username

--- scripts/fabfile.py
+++ scripts/fabfile.py
@@ -3,7 +3,7 @@
 #    - to invoke: fab -f file func()
 #    - $ fab -R local gen_key
 #    - $ fab -R dev push_key
-#    - $ fab -R dev test_key
+#    - $ fab -R dev test_key:username
 # NOTE: http://docs.fabfile.org/en/1.12/usage/env.html#roles
 
 # Import Fabric's API module#
@@ -16,9 +16,9 @@ import logging
 # As a good practice we can log the state of each phase in our script.
 #  https://docs.python.org/2.7/howto/logging.html
 logging.basicConfig(filename='check_ssh.log', level=logging.DEBUG)
-logging.debug('This message should go to the log file')
-logging.info('So should this')
-logging.warning('And this, too')
+logging.info('LOG STARTS')
+#logging.debug('This message should go to the log file')
+#logging.warning('And this, too')
 
 # Open the server list file and split the IP o each server.
 # http://www.tutorialspoint.com/python/string_split.htm
@@ -40,40 +40,44 @@ env.roledefs = {
 env.user = "vagrant"
 env.password = "vagrant"
 
-#vagrant@nagios-server:/vagrant/scripts$ fab -R local gen_key
 def gen_key():
-    with settings(warn_only=True):
-        username = prompt("Which USERNAME you like to GEN KEYS?")
+    with settings(warn_only=False):
+        usernameg = prompt("Which USERNAME you like to GEN KEYS?")
         print colored('#######################################################', 'blue')
         print colored('Consider that we generate user: username pass: username', 'blue')
         print colored('#######################################################', 'blue')
 
-        sudo('useradd -m -d /home/'+username+' -U '+username)
-        sudo('echo "'+username+':'+username+'" | chpasswd')
-        sudo("ssh-keygen -t rsa -f /home/" + username + "/.ssh/id_rsa -N ''", user=username)
+        sudo('useradd -m -d /home/'+usernameg+' -U '+usernameg)
+        sudo('echo "'+usernameg+':'+usernameg+'" | chpasswd')
+        sudo("ssh-keygen -t rsa -f /home/" + usernameg+ "/.ssh/id_rsa -N ''", user=usernameg)
         #http://unix.stackexchange.com/questions/36540/why-am-i-still-getting-a-password-prompt-with-ssh-with-public-key-authentication
-        sudo('chmod 755 /home/'+username)
-        sudo('chmod 755 /home/'+username+'/.ssh')
-        sudo('chmod 600 /home/'+username+'/.ssh/id_rsa')
+        #sudo('chmod 700 /home/' + usernameg)
+        sudo('chmod 755 /home/'+usernameg)
+        sudo('chmod 755 /home/'+usernameg+'/.ssh')
+        sudo('chmod 600 /home/'+usernameg+'/.ssh/id_rsa')
 
 def push_key():
-    with settings(warn_only=True):
-        username = prompt("Which USERNAME you like to CREATE & PUSH KEYS?")
-        sudo('useradd -m -d /home/' +username +' -U '+username)
-        sudo('echo "'+username+':'+username+'" | chpasswd')
-        local('ssh-copy-id -i /home/'+username+'/.ssh/id_rsa.pub '+username+'@'+env.host_string)
-        sudo('chmod 700 /home/'+username+'/.ssh/authorized_keys')
+    with settings(warn_only=False):
+        usernamep = prompt("Which USERNAME you like to CREATE & PUSH KEYS?")
+        sudo('useradd -m -d /home/' +usernamep +' -U '+usernamep)
+        sudo('echo "'+usernamep+':'+usernamep+'" | chpasswd')
+        # Remember that the usernamep is not in the remote server
+        # Then you are gona be ask the pass of this user.
+        # To avoid this you must use a user that it's already created
+        # in the local and remote host with the proper permissions.
+        local('ssh-copy-id -i /home/'+usernamep+'/.ssh/id_rsa.pub '+usernamep+'@'+env.host_string)
+        sudo('chmod 700 /home/'+usernamep+'/.ssh/authorized_keys')
 
-def test_key():
-    username = prompt("Which USERNAME you like to TEST KEYS?")
-    hostvm = sudo('hostname')
-    if (os.path.exists('/home/'+username+'/.ssh/')):
-        ssh_test = local('ssh -i /home/'+username+'/.ssh/id_rsa -q '+username+'@'+env.host_string+' exit')
-        if (ssh_test.succeeded):
-            print colored('###############################################', 'blue')
-            print colored(username+' WORKED! in:'+hostvm+' IP:'+env.host_string, 'blue')
-            print colored('###############################################', 'blue')
-    else:
-        print colored('##############################################', 'red')
-        print colored(username +' FAIL! in:'+hostvm+'- IP:'+env.host_string, 'red')
-        print colored('##############################################', 'red')
+def test_key(usernamet):
+    with settings(warn_only=False):
+        hostvm = sudo('hostname')
+        if (os.path.exists('/home/'+usernamet+'/.ssh/')):
+            ssh_test = local('ssh -i /home/'+usernamet+'/.ssh/id_rsa -o "StrictHostKeyChecking no" -q '+usernamet+'@'+env.host_string+' exit')
+            if (ssh_test.succeeded):
+                print colored('###################################################', 'blue')
+                print colored(usernamet+' WORKED! in:'+hostvm+' IP:'+env.host_string, 'blue')
+                print colored('###################################################', 'blue')
+        else:
+            print colored('###################################################', 'red')
+            print colored(usernamet+' FAIL! in:'+hostvm+'- IP:'+env.host_string, 'red')
+            print colored('###################################################', 'red')
